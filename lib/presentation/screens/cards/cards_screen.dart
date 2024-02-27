b240aadfdc1a07aca974c8ea8f5e5a22bc669b0e
HitMonChann <manuelsanchezsicilia44@gmail.com>
2024-02-27 18:29:29 +0100

    siguientes pantallas para nuevos componentes

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 const cards = <Map<String, dynamic>>[
   {'elevation': 0.0, 'label': 'Elevation 0'},
@@ -188,7 +187,6 @@ class _CardType4 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
     return Card(
       clipBehavior: Clip.hardEdge,
       elevation: elevation,
